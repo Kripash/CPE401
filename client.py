776cb815df0fcc305a4a7cc90bc82f0e84efd7dc
Kripash Shrestha <kripashs@gmail.com>
2019-02-22 21:33:58 -0800

    Initial commit

---
+++ client.py
@@ -0,0 +1,76 @@
+import sys
+import socket
+import SocketServer
+
+# from socket import *
+
+USER_ID = sys.argv[1]
+SERVER_IP = sys.argv[2]
+CLIENT_IP = "0.0.0.0"
+UDP_PORT = int(sys.argv[3])
+
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+# sock.bind((CLIENT_IP, UDP_PORT))
+
+# sock.sendto(MESSAGE, (SERVER_IP, UDP_PORT))
+
+# while True:
+#  data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+#  print "received message: ", data
+#  print "from :" , addr
+
+
+class UDPClient():
+  def __init__(self, user_id, server_ip, my_ip, udp_port):
+    self.user_id = user_id
+    self.server_ip = server_ip
+    self.my_ip = my_ip
+    self.udp_port = udp_port
+    self.UDP_server = (my_ip, udp_port)
+    self.message = "default message"
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    self.sock.bind((my_ip, udp_port))
+    self.data_received = False
+    self.data = "default message"
+    print("Client Sucessfully Initialized!")
+
+  def debug(self):
+    print("User ID: ", self.user_id)
+    print("Server IP: ", self.server_ip)
+    print("My IP: ", self.my_ip)
+    print("UDP Port: ", self.udp_port)
+
+  def sendMessageToServer(self):
+    self.message = raw_input("Message to send to server: ")
+    self.sock.sendto(self.message, (self.server_ip, self.udp_port))
+    self.data_received = False
+    self.waitForMessage()
+
+  def waitForMessage(self):
+    self.sock.settimeout(10)
+    try:
+      data, addr = self.sock.recvfrom(1024)
+      self.data_received = True
+    except socket.timeout:
+      self.data_received = False
+    if self.data_received:
+      print("Received reply from server: ", self.data)
+      print("Reply received from: ", addr)
+      self.data = data
+    if (not self.data_received):
+      print("No Reply from Server, Try Again!")
+
+  def actAsThread(self):
+    while True:
+      self.sendMessageToServer()
+
+
+def main():
+  UDP_client = UDPClient(sys.argv[1], sys.argv[2], "0.0.0.0", int(sys.argv[3]))
+  UDP_client.actAsThread()
+
+
+if __name__ == '__main__':
+  main()
+
---
+++ server.py
@@ -0,0 +1,40 @@
+import sys
+import socket
+import SocketServer
+
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+server_port = int(sys.argv[1])
+#print("Your Computer Name is:" + hostname)
+#print("Your Computer IP Address is:" + IPAddr)
+
+class UCHandler(SocketServer.BaseRequestHandler):
+  def handle(self):
+    remote = self.client_address
+    data, client = self.request
+    print(data)
+    client.sendto("Why did you say " + data + " ?", remote)
+
+def main():
+  UDP_server = ("0.0.0.0", server_port)
+  myserver = SocketServer.UDPServer(UDP_server, UCHandler)
+  myserver.serve_forever()
+
+if __name__ == '__main__':
+  main()
+
+
+#import socket
+#UDP_IP = "192.168.56.1"
+#UDP_PORT = 5006
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+#sock.bind((UDP_IP, UDP_PORT))
+
+#print("Server IP: ", UDP_IP)
+#print("Server Port: ", UDP_PORT)
+
+#while True:
+#  data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+#  print("received message: ", data)
+#  print("from :", addr)
+#  sock.sendto("Why did you say " + data +" ?" , addr)
